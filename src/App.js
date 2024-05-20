5ada26a00db39436a7d0199b74d5c1ae2e3b0457
bharath sethuraman <bharathsethuraman23@gmail.com>
2024-05-20 10:48:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 import Education from './Components/Education';
-import Certifications from './Components/Certifications';
+import Certifications from './Components/Cerifications';
 import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
-const TRACKING_ID = 'G-XXXXXXXXXX'; // Replace with your actual Tracking ID
-ReactGA.initialize(TRACKING_ID);
+// const TRACKING_ID = 'G-XXXXXXXXXX'; // Replace with your actual Tracking ID
+// ReactGA.initialize(TRACKING_ID);
 
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  // useEffect(() => {
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
 
   return (
     <div className="App">
